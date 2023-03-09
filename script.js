52661a62fa6d46cf62429b2e40c05dfa60e833be
anjumnihashaik <85540653+anjumnihashaik@users.noreply.github.com>
2023-03-09 22:33:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+    });
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Blogger", "Youtuber",],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
